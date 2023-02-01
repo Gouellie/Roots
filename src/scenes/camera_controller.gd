b14184f7c28d9eb2035f401fdbf0f200d035857b
Guillaume Ouellet <gulaou@gmail.com>
2023-02-01 18:55:40 +0800

    added camera control type
    use 'c' to toggle between different camera control type.
    Both axis free, x axis lock, y axis lick, no pan

--- src/scenes/camera_controller.gd
+++ src/scenes/camera_controller.gd
@@ -1,5 +1,7 @@
 extends Camera2D
 
+const default_zoom := Vector2(1.0,1.0)
+
 # how far in you can zoom
 var zoom_min   : Vector2 = Vector2(0.3, 0.3)
 # how far out you can zoom
@@ -13,6 +15,12 @@ var screen_start_position
 var dragging = false
 var lerping_to_selection = false
 
+var lock_x : bool = false
+var lock_y : bool = false
+
+
+var camera_control_type :int=0
+
 
 func _ready() -> void:
 	pass
@@ -22,7 +30,16 @@ func _process(_delta: float) -> void:
 	zoom = lerp(zoom, desired_zoom, 0.1)
 
 
+func _reset_camera() -> void:
+	zoom = default_zoom
+	desired_zoom = default_zoom
+	position = Vector2.ZERO
+
+
 func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("camera_control"):
+		_change_camera_control_type()
+		return
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == BUTTON_WHEEL_UP:
@@ -41,3 +58,29 @@ func _unhandled_input(event: InputEvent) -> void:
 			dragging = false
 	elif event is InputEventMouseMotion and dragging:
 		position = zoom * (mouse_start_pos - event.position) + screen_start_position
+		if lock_x:
+			position.x = 0.0
+		if lock_y:
+			position.y = 0.0
+
+
+func _change_camera_control_type() -> void:
+	_reset_camera()
+	camera_control_type = wrapi(camera_control_type+1, 0, 4)
+	match camera_control_type:
+		0:
+			lock_x = false
+			lock_y = false
+			print("camera control type : default")
+		1:
+			lock_x = true
+			lock_y = false
+			print("camera control type : x lock")		
+		2:
+			lock_x = false
+			lock_y = true
+			print("camera control type : y lock")
+		3:
+			lock_x = true
+			lock_y = true
+			print("camera control type : no pan")
