42cac7fe666598678dd82e40cff4f37079d812a9
Guillaume Ouellet <gulaou@gmail.com>
2023-02-03 00:00:40 +0800

    Added camera limit and background

--- src/scenes/camera_controller.gd
+++ src/scenes/camera_controller.gd
@@ -7,9 +7,12 @@ const default_zoom := Vector2(1.0,1.0)
 # how far in you can zoom
 var zoom_min   : Vector2 = Vector2(0.3, 0.3)
 # how far out you can zoom
-var zoom_max   : Vector2 = Vector2(3.0, 3.0)
+var zoom_max   : Vector2 = Vector2(2.5, 2.5)
 var zoom_speed : Vector2 = Vector2(0.2, 0.2)
 var desired_zoom : Vector2 = zoom
+
+var h_limits := Vector2(-900.0, 900.0)
+var w_limits := Vector2(-2500.0, 2500.0)
 
 var mouse_start_pos
 var screen_start_position
@@ -29,6 +32,8 @@ onready var camera_offset : Vector2
 
 func _ready() -> void:
 	camera_offset = OS.window_size * 0.5
+	w_limits += Vector2(camera_offset.x, camera_offset.x)
+	h_limits += Vector2(camera_offset.y, camera_offset.y)
 	var node = get_node(zoom_entity)
 	if node:
 		camera_center = node.position
@@ -71,6 +76,8 @@ func _unhandled_input(event: InputEvent) -> void:
 			position.x = camera_center.x - camera_offset.x
 		if lock_y:
 			position.y = camera_center.y - camera_offset.y
+		position.x = clamp(position.x, w_limits.x, w_limits.y)
+		position.y = clamp(position.y, h_limits.x, h_limits.y)
 
 
 func _change_camera_control_type() -> void:
