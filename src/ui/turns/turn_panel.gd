2d4064a8b898137ac18bb3b39cab70ea4feb8ca5
Leevani <ivogeelhoed@gmail.com>
2023-02-02 11:28:36 +0800

    implemented turn system into the entity base

--- src/ui/turns/turn_panel.gd
+++ src/ui/turns/turn_panel.gd
@@ -6,22 +6,26 @@ onready var progress_bar : ProgressBar = $ProgressBar
 onready var turn_label : Label = $Turn
 onready var step_label : Label = $Step
 
-func _ready():
+var turn_manager : TurnManager
+
+func _init():
 	Turns.connect("turn_manager_initialized", self, "on_turn_manager_initialized")
+	
+func _ready():
 	Turns.connect("step_next", self, "on_step_next")
 	Turns.connect("turn_next", self, "on_turn_next")
 	
-func on_turn_manager_initialized(_turn_manager):
-	var _tm : TurnManager = _turn_manager as TurnManager
-	if _tm == null:
-		Log.log_error(1, "No valid turn manager was initialized")
+	if turn_manager == null:
+		Log.log_error(1, "No valid TurnManager was initialized.")
 		return
-	
-	_max_turns = _tm.max_turns
+	_max_turns = turn_manager.max_turns
 	progress_bar.max_value = _max_turns	
-	progress_bar.value = _tm.turn
-	step_label.text = String(Turns.step_names[_tm.step])
-	turn_label.text = String(_tm.turn)
+	progress_bar.value = turn_manager.turn
+	step_label.text = String(Turns.step_names[turn_manager.step])
+	turn_label.text = String(turn_manager.turn)
+	
+func on_turn_manager_initialized(_turn_manager):
+	turn_manager = _turn_manager as TurnManager
 	
 func on_step_next(_step):
 	step_label.text = String(Turns.step_names[_step])
