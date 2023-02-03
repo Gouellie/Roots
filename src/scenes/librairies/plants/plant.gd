3db791a499cad602a9758cc28456d8b13addf4a3
Guillaume Ouellet <gulaou@gmail.com>
2023-02-03 09:23:08 +0800

    Updated plant disconnection checks
    - Now checking for unrooted plant (no root connection)
    - Only for now master plants

--- src/scenes/librairies/plants/plant.gd
+++ src/scenes/librairies/plants/plant.gd
@@ -2,9 +2,12 @@ extends Entity
 class_name Plant
 
 export (NodePath) var terrain_node_path
+export (bool) var is_master_plant
 
 var head_tile_cellv : Vector2
 var connected : bool = true setget set_is_connected,get_is_connected
+var unrooted : bool = false
+
 
 func _ready() -> void:
 	if terrain_node_path:
@@ -26,3 +29,15 @@ func set_is_connected(value : bool) -> void:
 
 func get_is_connected() -> bool:
 	return connected
+
+
+func _on_Area2D_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if not is_master_plant:
+		set_is_connected(true)
+		unrooted = false
+
+
+func _on_Area2D_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if not is_master_plant:	
+		set_is_connected(false)
+		unrooted = true
