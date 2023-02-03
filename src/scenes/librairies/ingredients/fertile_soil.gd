74c8fef06aeddfd4100970095edb01cf843bd018
Guillaume Ouellet <gulaou@gmail.com>
2023-02-03 19:31:09 +0800

    Updated fertile soil
    - New visuals
    - Ingredient remains when plant is spawned
    - added glowing orbs to help guide players

--- src/scenes/librairies/ingredients/fertile_soil.gd
+++ src/scenes/librairies/ingredients/fertile_soil.gd
@@ -2,24 +2,35 @@ extends Ingredient
 
 
 var _root_tile : Tile
+var ferlitized : bool = false
 
+onready var button := $Button
+onready var sprite_attention := $Sprite_Attention
 
 func _ready() -> void:
-	$Button.visible = false
+	button.visible = false
+	sprite_attention.visible = true
 
 
 func _process(delta: float) -> void:
+	if ferlitized:
+		return	
 	if is_instance_valid(_root_tile):
-		$Button.visible = _root_tile.connected
-
+		var show_button = _root_tile.connected
+		_toggle_visibility(show_button,not show_button)		
+	
 
 func _on_Area2D_area_entered(area: Area2D) -> void:
-	$Button.visible = true
+	if ferlitized:
+		return	
+	_toggle_visibility(true,false)
 	_root_tile = area.owner as Tile
 
 
 func _on_Area2D_area_exited(area: Area2D) -> void:
-	$Button.visible = false
+	if ferlitized:
+		return
+	_toggle_visibility(false,true)
 	_root_tile = area.owner	as Tile
 
 
@@ -29,4 +40,10 @@ func _on_Button_pressed() -> void:
 	if not _root_tile.connected:
 		return 
 	Events.emit_signal("spawn_plant", position)
-	queue_free()
+	ferlitized = true
+	_toggle_visibility(false,false)
+
+
+func _toggle_visibility(show_button, show_icon) -> void:
+	button.visible = show_button
+	sprite_attention.visible = show_icon
