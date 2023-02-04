a948b59ed560193b9b88027af808b154579e9df9
Guillaume Ouellet <gulaou@gmail.com>
2023-02-04 20:45:06 +0800

    updated name upgrades with proper convention
    Nutrient -> Soil

--- src/scenes/librairies/managers/upgrades_manager.gd
+++ src/scenes/librairies/managers/upgrades_manager.gd
@@ -3,7 +3,7 @@ class_name UpgradesManager
 
 var sun_level : int = 0
 var water_level : int = 0
-var nutrients_level : int = 0
+var soil_level : int = 0
 
 func _init() -> void:
 	Globals.upgrades_manager = self
@@ -12,7 +12,7 @@ func _init() -> void:
 func _ready() -> void:
 	Events.connect("shop_buy_sun_upgrade", self, "on_sun_upgraded")
 	Events.connect("shop_buy_water_upgrade", self, "on_water_upgraded")
-	Events.connect("shop_buy_soil_upgrade", self, "on_nutrients_upgraded")
+	Events.connect("shop_buy_soil_upgrade", self, "on_soil_upgraded")
 
 
 func on_sun_upgraded(level) -> void:
@@ -23,5 +23,5 @@ func on_water_upgraded(level) -> void:
 	water_level = level
 
 
-func on_nutrients_upgraded(level) -> void:
-	nutrients_level = level
+func on_soil_upgraded(level) -> void:
+	soil_level = level
